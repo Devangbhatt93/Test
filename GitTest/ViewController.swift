432faa2bbdfd2c09843af021237c9b2219259400
Dhaval Soni <dhaval.soni@SA-MAC-026.local>
2017-12-02 12:53:49 +0530

    asd

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("branchB")
+        print("branchBqq")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
